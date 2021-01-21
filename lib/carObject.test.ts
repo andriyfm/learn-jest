82d3fad5a12a7729d312afcccf7a264e32bd1b83
andriyfm <andrifmsyh@gmail.com>
2021-01-21 08:31:24 +0700

    Use ".not" for check the opposite of a matcher

--- lib/carObject.test.ts
+++ lib/carObject.test.ts
@@ -1,5 +1,5 @@
 import carObject from './carObject'
 
 test('carObject harus return object', () => {
-    expect(carObject()).toEqual({name: 'honda', type: 'HR-V'})
+    expect(carObject()).not.toEqual({name: 'honda', type: 'HR-V'})
 })
